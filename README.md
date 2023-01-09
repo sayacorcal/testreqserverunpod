08fd0d1c0c47008615af2325f4c1c4f93f8d4f0e
saidjoc@gmail.com <saidjoc@gmail.com>
2023-01-09 17:01:17 -0500

    update

--- README.md
+++ README.md
@@ -4,4 +4,3 @@ to run it you must follow the next steps:
 1) git clone https://github.com/sayacorcal/testreqserverunpod.git
 2) docker build -t server .
 3) docker run -d --name server1 -p 3000:3000 server
-
--- index.js
+++ index.js
@@ -1,14 +1,42 @@
+
 const express = require('express');
+const jwt = require('jsonwebtoken');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ message: 'home' });
+// Replace with your own secret key
+const secretKey = 'secretkey';
+
+// This route is protected and requires a valid token to access it
+app.get('/protected', (req, res) => {
+  // Get the token from the request headers
+  const token = req.headers['authorization'];
+  
+  // If there is no token, return a 401 unauthorized error
+  if (!token) {
+    return res.status(401).send({ error: 'No token provided' });
+  }
+
+  // Verify the token and decode the payload
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: 'Invalid token' });
+    }
+    
+    // If the token is valid, send a response with the user's data
+    res.send({ user: decoded.user });
+  });
 });
 
-app.get('/data', (req, res) => {
-  res.json({ message: 'Hello World' });
+// This route generates a new token
+app.get('/login', (req, res) => {
+  // Replace with your own user data
+  const user = { id: 1, name: 'John' };
+  
+  // Generate a new token with the user data and secret key
+  const token = jwt.sign({ user }, secretKey);
+
+  res.send({ token });
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+app.listen(3000, () => console.log('API listening on port 3000'));
