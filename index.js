bc7c7b1b68a6c9866e615e46af97c5b2795a4b37
saidjoc@gmail.com <saidjoc@gmail.com>
2023-01-13 16:06:32 -0500

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const app = express();
+
 const { exec } = require('child_process');
 exec('ls ', (err, stdout, stderr) => {
   if (err) {
@@ -13,12 +14,13 @@ exec('ls ', (err, stdout, stderr) => {
   console.log(`stdout: ${stdout}`);
   console.log(`stderr: ${stderr}`);
 });
-const indexRouter = require("./routes/index.js"); 
+//const indexRouter = require("./routes/index.js"); 
+
 //const bodyParser = require('body-parser');
 //app.use(bodyParser.json());
 app.use(express.json());
 
 
-app.use("/",indexRouter);
+//app.use("/",indexRouter);
 
 app.listen(3000, () => console.log('API listening on port 3000'));
