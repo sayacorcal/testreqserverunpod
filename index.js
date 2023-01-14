980098e84f1a1d1a14d00e8b2e857998990833ab
saidjoc@gmail.com <saidjoc@gmail.com>
2023-01-14 18:59:02 -0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
 const app = express();
-const indexRouter = require("./routes/index.js");
 
+const indexRouter = require("./routes/index.js"); 
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 app.use(express.json());
+
+
 app.use("/",indexRouter);
+
 app.listen(3000, () => console.log('API listening on port 3000'));
