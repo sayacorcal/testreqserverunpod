e7f9136e0dd69d951e59cd97cff2527103af25eb
saidjoc@gmail.com <saidjoc@gmail.com>
2023-01-15 14:37:58 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ const generateorders = require("../api/generateorders/routes.js");
 const login = require("../api/login/routes.js");
 
 var express = require("express");
+const { route } = require("../api/trainorders/routes.js");
 var router  = express.Router();
+
+router.use ( "/", (req, res)=>{
+    return res.status(200).send({ message: 'No Home yet' });
+})
 
 router.use ( "/trainorders",trainorders);
 router.use ( "/generateorders",generateorders);
