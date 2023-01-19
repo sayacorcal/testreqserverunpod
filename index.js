be7c48f8f2469ef23162b185b4dcf422ad9984c5
sayacorcal <sayacorcal@gmail.com>
2023-01-18 19:18:53 -0500

    removing the estra declaration of fs

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ if (fs.existsSync("/content")) {
 
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const fs = require('fs');
+//const fs = require('fs');
 const app = express();
 
 const indexRouter = require("./routes/index.js"); 
