03d2344f57cbae001259002bfd815cba14f7179e
sayacorcal <sayacorcal@gmail.com>
2023-01-20 14:22:35 -0500

    now fixing the error of not writing in the real ./orders.json file

--- api/endtrainorder/controller.js
+++ api/endtrainorder/controller.js
@@ -11,19 +11,21 @@ module.exports  = {
         }
         console.log(__dirname);
         console.log(process.cwd());
-
+        
         const orders = require('../../orders.json').orders;
         const id = req.body.id;
         const newStatus = req.body.status;
         console.log(id," ",newStatus)
+        
         for (let i = 0; i < orders.length; i++) {
-            
             if (orders[i].id === id) {
                 orders[i].status = newStatus;
+                fs.writeFileSync('../../orders.json', JSON.stringify(orders));
                 res.status(200).json({ message: 'Order status updated successfully' });
                 return;
             }
         }
+
         res.status(404).json({ message: 'Order not found' });
         //return res.status(200).send({ orders: lista });
     }
